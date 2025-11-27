154665acd4be2970b5a8b31ee3f41f1e9301682f
raean <rayyanarif6toa@gmail.com>
2025-11-27 02:51:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 8080,
+    strictPort: true,
+    allowedHosts: ['odontokaren-front-odontokaren.eis2pk.easypanel.host']
+  }
 })
